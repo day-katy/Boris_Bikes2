eea03beb7f2d24d017cbd9e8e894dfca7a5c3215
Katy Day <day.katy@gmail.com>
2021-01-06 16:57:32 +0000

    step 8 - adding release_bike method and rspec test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do 
+  it { is_expected.to respond_to(:release_bike) } 
+  # it "should release bike" do 
+  #   DockingStation.new respond_to(release_bike)
+  #   end
 end
