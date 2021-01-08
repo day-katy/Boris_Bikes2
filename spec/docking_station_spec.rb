56c7b1ee86fb91fb19e7e049d9203fc19eac073c
Katy Day <day.katy@gmail.com>
2021-01-08 15:58:20 +0000

    Added release_bike rspec and release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,29 +2,22 @@ require 'docking_station'
 
 describe DockingStation do
 
-  # it "should not release a bike if no bike is available" do
-  # expect {subject.release_bike}.to raise_exception
-  # puts "No bikes docked."
-  # end
+  describe "#release_bike" do
+  it "should respond to release_bike" do 
+    expect(DockingStation.new).to respond_to :release_bike
+    end
 
-  # it "should not dock a bike if no space is available" do
-  # bike = Bike.new
-  # subject.dock(bike)
-  #
-  # bike = Bike.new
-  # expect { subject.dock(bike) }.to raise_exception
-  # end
-  #
-  #
-  # # it "should release a working bike" do
-  # #   bike = subject.release_bike
-  # #   expect(bike.working?).to eq true
-  # # end
-  #
-  # it "should dock a bike" do
-  #  bike = Bike.new
-  #  expect(subject.dock(bike)).to eq bike
-  # end
+    it "should release a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it "should not release a bike if a bike is not there" do 
+      expect { subject.release_bike }.to raise_error "No bikes docked"
+    end
+  end
+
   describe '#dock' do
     it 'it responds to dock' do
       expect(DockingStation.new).to respond_to :dock
