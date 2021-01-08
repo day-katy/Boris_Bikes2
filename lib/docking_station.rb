73349d931410e36a9e04e751c82341309dcd91af
Katy Day <day.katy@gmail.com>
2021-01-08 16:42:51 +0000

    Completed stage 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,12 +7,23 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bikes docked" if @bikes.empty?
+    fail "No bikes docked" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking station full" if @bikes.count >= 20
+    fail "Docking station full" if full?
     @bikes.push(bike)
     end
+
+    private
+    
+    def full?
+      @bikes.count >= 20
   end
+
+  def empty?
+    @bikes.empty?
+  end
+
+end
